e3b183d368faca578b274ab0689d9b63b0e3c976
Stefan Seifried <s.seifried@gmx.at>
2017-11-19 15:47:47 +0100

    added first version of exifrename

---
+++ exifrename.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+# rename all images according to their exif date with ascending numbers.
+#
+#
+# array of considered filetypes
+#
+FILETYPES=("*.jpg");
+#
+#
+###############################################################################
+# Check prerequisites
+I=`which exiftool`
+if [ "$I" == "" ]; then
+  echo "The 'exiftool' command is missing or not available."
+  exit 1
+fi;
+###############################################################################
+# Scanning loop
+for file in $FILETYPES; do
+    date_str="$(
+        exiftool -d %Y%m%d_%H%M%S $file |
+        awk -F': ' '/Create Date/{print $2}'
+        )"
+    filetype="$(echo $file|awk -F . '{print $NF}')"
+    echo "renaming $file to $date_str.$filetype"
+
+    mv "$file" "$date_str.$filetype"
+done;
